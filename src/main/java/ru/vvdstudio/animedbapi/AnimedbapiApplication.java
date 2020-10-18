b0bd0d2f7826ea6434f5d92c46d325e379ce0090
lsnkf <lsnkf@yandex.ru>
2020-10-18 15:35:44 +0300

    ANIMEAPP-5 Start

---
+++ src/main/java/ru/vvdstudio/animedbapi/AnimedbapiApplication.java
@@ -0,0 +1,13 @@
+package ru.vvdstudio.animedbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimedbapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimedbapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vvdstudio/animedbapi/AnimedbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vvdstudio.animedbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimedbapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
